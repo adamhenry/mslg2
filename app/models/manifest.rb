df1a3f14e7c1e23737e23d586a099d3b9033a555
Adam Henry <a.david.henry@gmail.com>
2008-06-10 18:42:53 -0700

    added manifest duplication hadeling

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -1,4 +1,18 @@
 class Manifest < ActiveRecord::Base
 	belongs_to :ship
 	belongs_to :item
+
+	def duplicate_manifest
+		ship.manifests.detect { |m| m.item = item }
+	end
+
+	def merge(old)
+		self.created_at = old.created_at
+		self.number += old.number
+		old.destroy
+	end
+
+	def before_save
+		merge(duplicate_manifest) if duplicate_manifest
+	end
 end
